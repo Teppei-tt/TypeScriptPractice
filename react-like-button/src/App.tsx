98f59e5607ec2ec6ff16eb839d9d732512a2dbdd
Teppei <teppei.takata03@gmail.com>
2023-06-03 13:25:05 +0900

    LikeButton実装

--- react-like-button/src/App.tsx
+++ react-like-button/src/App.tsx
@@ -3,9 +3,16 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">TypeScriptはいいぞ</header>
+            <header className="App-header">
+                <LikeButton />
+            </header>
         </div>
     );
 }
 
 export default App;
+
+function LikeButton() {
+    const count = 999;
+    return <span>♥ {count}</span>;
+}
