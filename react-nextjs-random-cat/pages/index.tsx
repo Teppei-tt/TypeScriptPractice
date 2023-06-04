55c900e82f97882a0c20e476a071df6fd1948cfa
Teppei <teppei.takata03@gmail.com>
2023-06-04 15:57:41 +0900

    fetchAPIを使用しての実装

--- react-nextjs-random-cat/pages/index.tsx
+++ react-nextjs-random-cat/pages/index.tsx
@@ -1,6 +1,40 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-    return <div>猫画像予定地</div>;
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    };
+
+    return (
+        <>
+            <button onClick={handleClick}>他のニャンコをみる</button>
+            <div>{loading || <img src={imageUrl} />}</div>
+        </>
+    );
 };
 export default IndexPage;
+
+type Image = {
+    url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
